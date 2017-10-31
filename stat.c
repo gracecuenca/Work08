64fc000eb5c159a2187ed202978a22dd13901174
gracecuenca <gcuenca@stuy.edu>
2017-10-31 17:41:37 -0400

    fixed lol

--- stat.c
+++ stat.c
@@ -6,43 +6,17 @@
 #include <errno.h>
 #include <time.h>
 
-int return_size(char * file_name){
-  struct stat sb;
-  if(errno){
-    printf("%s\n", strerror(errno));
-  }
-  printf("size of file: %lld\n", sb.st_size);
-  return 0;
-}
-
-int return_mode(char * file_name){
-  struct stat sb;
-  if(errno){
-    printf("%s\n", strerror(errno));
-  }
-  printf("permissions of the file: %o\n", sb.st_mode);
-  return 0;
-}
-
-char * return_time(char * file_name){
-  struct stat sb;
-  if(errno){
-    printf("%s\n", strerror(errno));
-  }
-  printf("%s\n", ctime(&sb.st_atime));
-  return ctime(&sb.st_atime);
-}
+//work
 
 int main(){
-  return_size("stat.c");
-  return_mode("stat.c");
-  return_time("stat.c");
-  return 0;
+  struct stat sb;
+  stat("stat.c", &sb);
+  printf("size of file: %lld\n", sb.st_size);
+  printf("permissions of the file: %o\n", sb.st_mode);
+  printf("%s\n", ctime(&sb.st_atime));
 
-  char mystring[200];
-
-  printf("printing out the size for human eyes\n");
-  sprintf(mystring, "%s", return_time("stat.c"));
-
+  printf("File Permissions:\n");
   
+
+
 }
